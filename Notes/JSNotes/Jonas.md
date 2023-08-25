039a07dd7952441bd40d40f50974e039eac9db6e
anurg <anurg@yahoo.com>
2023-08-25 19:48:54 +0530

    JS learning

---
+++ Notes/JSNotes/Jonas.md
@@ -0,0 +1,3 @@
+### Notes from Jonas JS course
+
+https://rgbk21.github.io/Code/JavaScript/JavaScript.html
---
+++ Notes/Projects/JSBasics/script.js
@@ -0,0 +1,42 @@
+let country = "India";
+let continent = "Asia";
+let population = 1360000000;
+let finPopulation = 600000000000;
+let isIsland = false;
+let language = ["Hindi", "English", "Sanskrit", "Punjabi"];
+// console.log(country);
+// console.log(continent);
+// population++;
+// console.log(population);
+
+// console.log(isIsland);
+// console.log(language);
+// language.forEach(function (x) {
+//   console.log("We speak " + x);
+// });
+// if (isIsland != true) {
+//   let string = `${country} is not an Island`;
+//   console.log(string);
+// }
+// if (finPopulation >= population) {
+//   console.log(`Finland has more population than ${country}`);
+// } else {
+//   console.log(`${country} has more poluation than Finland.`);
+// }
+// let i = 10;
+// while (i > 0) {
+//   console.log(i);
+//   i--;
+// }
+// for (i = 10; i > 0; i--) {
+//   console.log(i);
+// }
+console.log(
+  `${country} is in ${continent} and its population is ${population}`
+);
+console.log("hello", "Anurag");
+// console.log("9" - "5");
+// console.log("19" - "13" + "17");
+// console.log("19" - "13" + 17);
+// console.log("123" < 57);
+// console.log(5 + 6 + "4" + 9 - 4 - 2);
