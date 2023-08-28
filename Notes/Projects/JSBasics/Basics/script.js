f95795cadea67c90517e42d04a4b617590e95944
anurg <anurg@yahoo.com>
2023-08-28 14:54:49 +0530

    JS Learning.

--- Notes/Projects/JSBasics/Basics/script.js
+++ Notes/Projects/JSBasics/Basics/script.js
@@ -1,81 +1,9 @@
-// "use strict";
-// alert("Hello");
+"use strict";
+// const myArray = [0, 1, 2, 3];
 
-// console.log(student1);
-// console.log(student2);
-// console.log(student3);
-var student1 = "Alice";
-let student2 = "Bob";
-const student3 = "Charlie";
-// console.log(add_Declaration(2, 3));
-// console.log(add_Expression_with_var); // Prints: undefined
+// const [a, b, c, d] = myArray;
 
-// function add_Declaration(a, b) {
-//   return a + b;
-// }
-
-// const add_Expression = function (a, b) {
-//   return a + b;
-// };
-
-// const add_ArrowFunction = (a, b) => a + b;
-
-// var add_Expression_with_var = function (a, b) {
-//   return a + b;
-// };
-
-// console.log(this);
-// const calcAge = function (currYear, birthYear) {
-//   console.log(this);
-//   return currYear - birthYear;
-// };
-
-// calcAge(2023, 1973);
-
-// const dave = {
-//   birthYear: 1981,
-//   calcAgeDave: function (currYear) {
-//     console.log(this);
-//     console.log(currYear - this.birthYear);
-//   },
-// };
-
-// dave.calcAgeDave(2023, 1973);
-
-// const eve = {
-//   birthYear: 2000,
-// };
-
-// eve.calcAgeEve = dave.calcAgeDave;
-// eve.calcAgeEve(2023);
-
-// const myFunc = dave.calcAgeDave;
-// myFunc(2023);
-
-// const felicia = {
-//   firstName: "Felicia",
-
-//   // Avoid using arrow functions as methods since they can lead to errors
-//   greet: () => {
-//     console.log(this);
-//     console.log(`Hi ${this.firstName}`);
-//   },
-
-//   // Prefer function expressions as methods instead
-//   greet2: function () {
-//     console.log(this); // Now 'this' refers to the object calling the method, which is felicia
-//     console.log(`Hi ${this.firstName}`);
-//   },
-// };
-// felicia.greet(); // Prints: undefined
-// felicia.greet2(); // Prints: 'Hi Felicia'
-
-// const anotherSummation = function (a, b) {
-//   console.log(arguments);
-//   // 'arguments' is an array. Hence we could cycle through each element and do computations on the arguments if required
-//   return a + b;
-// };
-// anotherSummation(2, 3, 4, 5, 6);
+// console.log(a, b, c, d);
 
 const restaurant = {
   name: "Classico Italiano",
@@ -88,7 +16,6 @@ const restaurant = {
   order: function (starterIdx, mainIdx) {
     return [this.starterMenu[starterIdx], this.mainMenu[mainIdx]];
   },
-
   openingHours: {
     thu: {
       open: 12,
@@ -105,9 +32,74 @@ const restaurant = {
   },
 };
 
-// The variables names should exactly match the property names that we want to retrieve from the object
-// Unlike destructuring in arrays, we do not need to specify all the properties
-// and the properties can also be specified out-of-order.
-// Just like arrays, this now creates 3 new variables
-const { name, openingHours, categories } = restaurant;
-console.log(name, openingHours, categories); // Prints the values from the restaurant object
+const [first, second] = restaurant.starterMenu;
+// console.log(first, second);
+
+let [ctgry1, , ctgry3] = restaurant.categories;
+// console.log(ctgry1, ctgry3);
+[ctgry3, ctgry1] = [ctgry1, ctgry3];
+// console.log(ctgry1, ctgry3);
+
+let [starter, main] = restaurant.order(1, 2);
+// console.log(starter, main);
+
+// const nestedArray = [1, 2, [3, 4]];
+// const [a, b, [c, d]] = nestedArray;
+// console.log(a, b, c, d);
+
+// let [p, q, r] = [2, 3];
+// console.log(p, q, r); // Prints: 2 3 undefined
+// // But now we assign default values for each variable
+// [p = 1, q = 1, r = 1] = [2, 3];
+// console.log(p, q, r); // Prints: 2 3 1
+
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+// const {
+//   name: restaurantName,
+//   openingHours: workingHours,
+//   categories: tags,
+//   menu = [],
+// } = restaurant;
+// console.log(restaurantName, workingHours, tags, menu);
+
+let amt1 = 100;
+let amt2 = 200;
+const myObject = { amt1: 1, amt2: 2 };
+
+({ amt1, amt2 } = myObject);
+const workingHours = {
+  fri: {
+    open: 11,
+    close: 23,
+  },
+};
+
+const {
+  fri: { open, close },
+} = workingHours;
+console.log(open, close);
+
+const printPersonDetailsUsingObj = function (person) {
+  console.log(
+    `Hi, ${person.name} ! As per our record, you were born in ${person.birthPlace} and have a job ${person.job}`
+  );
+};
+
+const printPersonDetailsUsingObjDec = function ({ name, birthPlace, job }) {
+  console.log(
+    `Hi, ${name} ! As per our record, you were born in ${birthPlace} and have a job ${job}`
+  );
+};
+
+const alice = {
+  name: "Alice",
+  dob: "1/1/1990",
+  birthPlace: "New York",
+  job: "Teacher",
+  hobbies: ["rock climbing", "pottery"],
+};
+
+// printPersonDetailsUsingObj(alice);
+// printPersonDetailsUsingObjDec(alice);
