5bf15d0a9ada17d8473c2bd51b1375e6399c0755
anurg <anurg@yahoo.com>
2024-01-31 15:31:22 +0530

    webdev notes.

--- NareshIT_webdev/node-examples/todo-api.js
+++ NareshIT_webdev/node-examples/todo-api.js
@@ -47,6 +47,80 @@ app.post("/register-user", (request, response) => {
   });
 });
 
+app.get("/appointments/:userid", (request, response) => {
+  mongoClient.connect(conStr).then((clientObject) => {
+    const database = clientObject.db("tododb");
+    database
+      .collection("appointments")
+      .find({ UserId: request.params.userid })
+      .toArray()
+      .then((documents) => {
+        response.send(documents);
+        response.end();
+      });
+  });
+});
+
+app.post("/add-task", (request, response) => {
+  const task = {
+    Id: parseInt(request.body.Id),
+    Title: request.body.Title,
+    Date: new Date(request.body.Date),
+    Description: request.body.Description,
+    UserId: request.body.UserId,
+  };
+
+  mongoClient.connect(conStr).then((clientObject) => {
+    const database = clientObject.db("tododb");
+    database
+      .collection("appointments")
+      .insertOne(task)
+      .then(() => {
+        response.send("Appointment Added!");
+        response.end();
+      });
+  });
+});
+
+app.put("/edit-task/:id", (request, response) => {
+  var id = parseInt(request.params.id);
+  mongoClient.connect(conStr).then((clientObject) => {
+    const database = clientObject.db("tododb");
+    database
+      .collection("appointments")
+      .updateOne(
+        { Id: id },
+        {
+          $set: {
+            Id: parseInt(request.body.Id),
+            Title: request.body.Title,
+            Date: new Date(request.body.Date),
+            Description: request.body.Description,
+            UserId: request.body.UserId,
+          },
+        }
+      )
+      .then(() => {
+        response.send("Record updated.");
+        response.end();
+      });
+  });
+});
+
+app.delete("/delete-task/:id", (request, response) => {
+  const id = parseInt(request.params.id);
+  mongoClient.connect(conStr).then((clientObject) => {
+    const database = clientObject.db("tododb");
+    database
+      .collection("appointments")
+      .deleteOne({ Id: id })
+      .then(() => {
+        console.log("Task Deleted!");
+        response.send("Record Deleted!");
+        response.end();
+      });
+  });
+});
 app.listen(6060);
 console.log("Server started at http://127.0.0.1:6060");
 
