264012b59ea2aecf50fdc5b58621f1c305a728d2
anurg <anurg@yahoo.com>
2024-02-11 19:17:27 +0530

    commit.

--- react-projects/shopping-react/src/components/login/login.jsx
+++ react-projects/shopping-react/src/components/login/login.jsx
@@ -1,1 +1,22 @@
 import "./login.css";
+
+export default function Login() {
+  return (
+    <div className="form-container">
+      <form>
+        <h3> User Login</h3>
+        <dl>
+          <dt>User Id</dt>
+          <dd>
+            <input type="text" />
+          </dd>
+          <dt>Password</dt>
+          <dd>
+            <input type="password" />
+          </dd>
+        </dl>
+        <button>Login</button>
+      </form>
+    </div>
+  );
+}
