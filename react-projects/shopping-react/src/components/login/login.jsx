f6acfa01b4ff577d19fff86c5ed83082a2a8c17c
anurg <anurg@yahoo.com>
2024-02-11 19:34:29 +0530

    Notes fomratting.

--- react-projects/shopping-react/src/components/login/login.jsx
+++ react-projects/shopping-react/src/components/login/login.jsx
@@ -2,21 +2,41 @@ import "./login.css";
 
 export default function Login() {
   return (
-    <div className="form-container">
-      <form>
-        <h3> User Login</h3>
-        <dl>
-          <dt>User Id</dt>
-          <dd>
-            <input type="text" />
-          </dd>
-          <dt>Password</dt>
-          <dd>
-            <input type="password" />
-          </dd>
-        </dl>
-        <button>Login</button>
-      </form>
+    <div>
+      <header className="mt-3">
+        <button
+          data-bs-target="#navbar"
+          data-bs-toggle="offcanvas"
+          className="btn btn-dark bi bi-justify"
+        ></button>
+        <div className="offcanvas offcanvas-start" id="navbar" tabIndex="-1">
+          <div className="offcanvas-header">
+            <h3>Navbar</h3>
+            <button
+              className="btn btn-close"
+              data-bs-dismiss="offcanvas"
+            ></button>
+          </div>
+          <div className="offcanvas-body">..your menu..</div>
+        </div>
+      </header>
+      <div className="form-container">
+        <form className="alert alert-warning alert-dismissible">
+          <h3 className="bi bi-person-fill">User Login</h3>
+          <button className="btn btn-close" data-bs-dismiss="alert"></button>
+          <dl>
+            <dt>User Id</dt>
+            <dd>
+              <input type="text" className="form-control" />
+            </dd>
+            <dt>Password</dt>
+            <dd>
+              <input type="password" className="form-control" />
+            </dd>
+          </dl>
+          <button className="btn btn-warning w-100">Login</button>
+        </form>
+      </div>
     </div>
   );
 }
--- react-projects/shopping-react/src/index.js
+++ react-projects/shopping-react/src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Login from "./components/login/login";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
+import * as bootstrap from "bootstrap";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
