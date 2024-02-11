9e23c751aa91059d388b5c1880dd75291c89dd74
anurg <anurg@yahoo.com>
2024-02-11 22:01:53 +0530

    React Notes 3rd Feb 2024.

---
+++ react-projects/shopping-react/src/components/data-binding/data-binding.jsx
@@ -0,0 +1,9 @@
+export function DataBinding() {
+  //   var price = prompt("Please Enter price:");
+  return (
+    <div className="container-fluid">
+      <h2>Data Types</h2>
+      {/* {price === null ? "Please Enter Price" : price} */}
+    </div>
+  );
+}
