b3b2560e96f0bf1d83ab3c2da8e75417e27c8256
anurg <anurg@yahoo.com>
2024-02-11 22:32:33 +0530

    formatting changes.

--- react-projects/shopping-react/src/components/data-binding/data-binding.jsx
+++ react-projects/shopping-react/src/components/data-binding/data-binding.jsx
@@ -1,9 +1,39 @@
 export function DataBinding() {
-  //   var price = prompt("Please Enter price:");
+  var categories = ["All", "Footwear", "Mobiles", "Electronics"];
   return (
     <div className="container-fluid">
       <h2>Data Types</h2>
-      {/* {price === null ? "Please Enter Price" : price} */}
+      <ol>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
+      <select>
+        {categories.map((category) => (
+          <option key={category} value={category}>
+            {" "}
+            {category}
+          </option>
+        ))}
+      </select>
+      <br />
+      <ul className="list-unstyled">
+        {categories.map((category) => (
+          <li key={category}>
+            {" "}
+            <input type="checkbox" /> <label>{category}</label>{" "}
+          </li>
+        ))}
+      </ul>
+      <br />
+      <nav className="p-2 border d-flex justify-content-between border-1 border-dark">
+        {categories.map((category) => (
+          <a className="btn btn-dark" key={category} href="#">
+            {" "}
+            {category}{" "}
+          </a>
+        ))}
+      </nav>
     </div>
   );
 }
