bf038f02cb8ba13f4dba44e4a6336ef02aae6cef
anurg <anurg@yahoo.com>
2024-02-12 14:42:00 +0530

    formatting.

--- react-projects/shopping-react/src/components/data-binding/data-binding.jsx
+++ react-projects/shopping-react/src/components/data-binding/data-binding.jsx
@@ -1,32 +1,12 @@
 export function DataBinding() {
-  var menu = [
-    { Category: "Electronics", Products: ["TV", "Mobiles"] },
-    { Category: "Footwear", Products: ["Boots", "Sneakers"] },
-  ];
+
+  var regExp = /\+91\d{10}/;
+var value = "+919876543210";
+
   return (
     <div className="container-fluid">
       <h2>Data Types:</h2>
-      <ol>
-        {menu.map((item) => (
-          <li key={item.Category}>
-            {item.Category}
-            <ul>
-              {item.Products.map((product) => (
-                <li key={product}>{product}</li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ol>
-      <select>
-        {menu.map((item) => (
-          <optgroup key={item.Category} label={item.Category}>
-            {item.Products.map((product) => (
-              <option key={product}>{product}</option>
-            ))}
-          </optgroup>
-        ))}
-      </select>
+      { (value.match(regExp)) ? "Valid" : "Invalid" }
     </div>
   );
 }
