bebd41a155ef14df0923e7d08e341b2debe7256a
anurg <anurg@yahoo.com>
2024-02-12 23:03:13 +0530

    practice.

--- react-projects/shopping-react/src/components/data-binding/data-binding.jsx
+++ react-projects/shopping-react/src/components/data-binding/data-binding.jsx
@@ -1,17 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export function DataBinding() {
-
-const [categories, setCategories] = useState(["All", "Electronics", "Footwear"]);
-
+  const [name, setName] = useState("");
+  useEffect(() => {
+    setName("Anurag");
+  }, []);
   return (
     <div className="container-fluid">
       <h2>Data Types:</h2>
-      <ol>
-      {
-        categories.map(category=><li key={category}>{category}</li>)
-      }
-      </ol>
+      Hello ! {name}
     </div>
   );
 }
