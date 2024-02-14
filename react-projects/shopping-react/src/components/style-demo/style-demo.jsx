7c5f2e40b5bf353651423c78ca903ef406ce1c21
anurg <anurg@yahoo.com>
2024-02-14 12:00:33 +0530

    Style Binding & Class Binding Demo.

--- react-projects/shopping-react/src/components/style-demo/style-demo.jsx
+++ react-projects/shopping-react/src/components/style-demo/style-demo.jsx
@@ -25,27 +25,64 @@ import "./style-demo.css"
 // }
 
 // CLass binding Example
-export function StyleDemo() {
-    const [nameError, setNameError] = useState('invalid-style');
-    const [required, setRequired] = useState('d-block text-danger')
-    function handleNameError(e) {
-        if (e.target.value ==='') {
-           setNameError('invalid-style');
-           setRequired('d-block text-danger')
-        } else {
-            setNameError('valid-style');
-            setRequired('d-none')
-        }
-    }
-    return (
-        <div className="container-fluid">
-            <dl>
-                <dt>User Name</dt>
-                <dd><input type="text" className={nameError} onChange={handleNameError}/></dd>
-                <dd className={required}>Input Required</dd>
-            </dl>
-        </div>
-    );
-}
+// export function StyleDemo() {
+//     const [nameError, setNameError] = useState('invalid-style');
+//     const [required, setRequired] = useState('d-block text-danger')
+//     function handleNameError(e) {
+//         if (e.target.value ==='') {
+//            setNameError('invalid-style');
+//            setRequired('d-block text-danger')
+//         } else {
+//             setNameError('valid-style');
+//             setRequired('d-none')
+//         }
+//     }
+//     return (
+//         <div className="container-fluid">
+//             <dl>
+//                 <dt>User Name</dt>
+//                 <dd><input type="text" className={nameError} onChange={handleNameError}/></dd>
+//                 <dd className={required}>Input Required</dd>
+//             </dl>
+//         </div>
+//     );
+// }
 
 // Style Binding Another example.
+
+
+
+export function StyleDemo(){
+
+
+    const [theme, setTheme] = useState('bg-light text-dark');
+    const [btnStyle, setBtnStyle] = useState('btn btn-dark w-100');
+
+    function ThemeChange(e){
+        if(e.target.checked){
+            setTheme('bg-dark text-white');
+            setBtnStyle('btn btn-light w-100');
+        } else {
+            setTheme('bg-light text-dark');
+            setBtnStyle('btn btn-dark w-100');
+        }
+    }
+
+    return(
+        <div className="container-fluid d-flex justify-content-center align-items-center" style={{height:'100vh'}}>
+            <form className={theme}>
+                <div className="form-switch">
+                    <input type="checkbox" onChange={ThemeChange} className="form-check-input" /> <label className="form-check-label"> Dark Mode </label>
+                </div>
+                <h2 className="bi bi-person-fill">User Login</h2>
+                <dl>
+                    <dt>User Name</dt>
+                    <dd><input type="text" className="form-control" /></dd>
+                    <dt>Password</dt>
+                    <dd><input type="password" className="form-control" /></dd>
+                </dl>
+                <button className={btnStyle}>Login</button>
+            </form>
+        </div>
+    )
+}
