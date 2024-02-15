979971e619b1d8c45fc5b37e312bd23c7a9a8120
anurg <anurg@yahoo.com>
2024-02-15 10:49:56 +0530

    onMouseOver event demo.

--- react-projects/shopping-react/src/components/event-demo/event-demo.jsx
+++ react-projects/shopping-react/src/components/event-demo/event-demo.jsx
@@ -1,14 +1,38 @@
+import axios from "axios";
+
+import { useEffect, useState } from "react";
+import "./even-demo.css";
 
 export function EventDemo() {
-    function handleSubmit(e) {
-        alert("Later we will post data to API.")
-        e.preventDefault();
+    const [mobiles, setMobiles] = useState([]);
+    const [targetImg, setTargetImg] = useState();
+    function LoadMobiles() {
+        axios.get("mobile.json").then((response)=>setMobiles(response.data)).catch((ex)=>console.log(ex));
+    }
+    useEffect(()=>{
+        LoadMobiles();
+    },[])
+    
+    function handleMouseOver(e) {
+        setTargetImg(e.target.src);
     }
     return (
         <div className="container-fluid m-2 p-4" >
-           <form className="w-50" noValidate>
-            User Name: <input type="email"  name="UserName"/> <button type="submit">Submit</button>
-           </form>
-        </div>
+           <section className="mt-4 row">
+                <nav className="col-1">
+                    {
+                        mobiles.map((mobile)=>
+                            <div  key={mobile.img_src}>
+                                <img src={mobile.img_src} width="60" height="100" onMouseOver={handleMouseOver} />
+                            </div>
+                        
+                        )
+                    }
+                </nav>
+                <main className="col-11">
+                    <img width="300" height="400" src={targetImg} />
+                </main>
+           </section>
+          </div> 
     );
 }
