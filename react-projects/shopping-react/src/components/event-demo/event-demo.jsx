27cccddc02dec5164cd4ab4095a623a27a5f7586
anurg <anurg@yahoo.com>
2024-02-15 13:07:27 +0530

    Strong Password Demo.

--- react-projects/shopping-react/src/components/event-demo/event-demo.jsx
+++ react-projects/shopping-react/src/components/event-demo/event-demo.jsx
@@ -84,48 +84,80 @@ import axios from "axios";
 // }
 
 // Key events
+// export function EventDemo(){
+//     const [userId,setUserId] = useState('');
+//     const [users,setUsers] = useState([]);
+//     const [userError,setUserError] = useState('');
+//     const [valClass, setValClass] = useState('');
+//     const [showWarn, setShowWarn] = useState('d-none');
+//     useEffect(()=>{
+//        axios.get("users.json").then((response)=>{
+//             setUsers(response.data);
+//        }) 
+//     },[])
+//     function VerifyUserId(e) {
+//         for (var user of users) {
+//             if (user.UserId === e.target.value) {
+//                 setUserError("User Id taken, Please try another.");
+//                 setValClass('text-danger');
+//                 break;
+//             } else {
+//                 setUserError("User Id Available");
+//                 setValClass('text-success');
+//             }
+//         }
+//     }
+//     function VerifyCaps(e) {
+//         if (e.which >=65 && e.which<=90) {
+//             setShowWarn('d-block');
+//         } else {
+//             setShowWarn('d-none');
+//         }
+//     }
+//     return (
+//         <div className="container-fluid m-2" >
+//             <h2>Register User</h2>
+//             <dl>
+//                 <dt>User Id</dt>
+//                 <dd><input type="text" onKeyUp={VerifyUserId} /></dd>
+//                 <dd className={valClass}>{userError}</dd>
+//                 <dt>Password</dt>
+//                 <dd><input type="password" onKeyPress={VerifyCaps}/></dd>
+//                 <dd className={`text-warning ${showWarn}`}><span className="bi bi-exclamation-triangle-fill text-warning"></span>
+//                     Warning- Caps On</dd>
+//             </dl>
+//         </div>
+//      );
+// }
+
+//  Password Strength Demo
 export function EventDemo(){
-    const [userId,setUserId] = useState('');
-    const [users,setUsers] = useState([]);
-    const [userError,setUserError] = useState('');
-    const [valClass, setValClass] = useState('');
-    const [showWarn, setShowWarn] = useState('d-none');
-    useEffect(()=>{
-       axios.get("users.json").then((response)=>{
-            setUsers(response.data);
-       }) 
-    },[])
-    function VerifyUserId(e) {
-        for (var user of users) {
-            if (user.UserId === e.target.value) {
-                setUserError("User Id taken, Please try another.");
-                setValClass('text-danger');
-                break;
+    const [msg, setMsg] = useState('');
+    const [meter, setMeter] = useState('');
+
+    function VerifyPassword(e) {
+        if (e.target.value.match(/(?=.*[A-Z])\w{4,15}/)) {
+            setMeter(100);
+            setMsg("Strong Password")
+        } else {
+            if (e.target.value.length<4) {
+                setMeter(30);
+                setMsg('Poor Pasword'); 
             } else {
-                setUserError("User Id Available");
-                setValClass('text-success');
+                setMeter(70);
+                setMsg('Weak Pasword'); 
             }
         }
     }
-    function VerifyCaps(e) {
-        if (e.which >=65 && e.which<=90) {
-            setShowWarn('d-block');
-        } else {
-            setShowWarn('d-none');
-        }
-    }
     return (
-        <div className="container-fluid m-2" >
+        <div className="container-fluid m-2 p-4">
             <h2>Register User</h2>
             <dl>
-                <dt>User Id</dt>
-                <dd><input type="text" onKeyUp={VerifyUserId} /></dd>
-                <dd className={valClass}>{userError}</dd>
                 <dt>Password</dt>
-                <dd><input type="password" onKeyPress={VerifyCaps}/></dd>
-                <dd className={`text-warning ${showWarn}`}><span className="bi bi-exclamation-triangle-fill text-warning"></span>
-                    Warning- Caps On</dd>
+                <dd><input type="password" onKeyUp={VerifyPassword}/></dd>
+                <dd><meter min="1" max="100" value={meter} style={{width : "200px"}}></meter></dd>
+                <dd>{msg}</dd>
             </dl>
         </div>
-     );
+    );
 }
