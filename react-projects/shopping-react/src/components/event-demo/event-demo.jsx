5d553ee2682a384769b92383cc3c8cf9d75c4546
anurg <anurg@yahoo.com>
2024-02-15 12:38:42 +0530

    Key Events.

--- react-projects/shopping-react/src/components/event-demo/event-demo.jsx
+++ react-projects/shopping-react/src/components/event-demo/event-demo.jsx
@@ -38,7 +38,8 @@
 
 // Flag animation cursor Demo
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 // export function EventDemo(){
 
@@ -63,21 +64,57 @@ import { useState } from "react";
 // }
 
 //  marquee demo
+// export function EventDemo(){
+//     function handleMouseOver(e) {
+//         e.target.stop();
+//     }
+//     function handleMouseOut(e) {
+//         e.target.start();
+//     }
+//     return (
+//         <div className="container-fluid m-2 p-4">
+//             <marquee scrollamount="20" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} style={{padding:'100px'}} >
+//                 <img src="m1.png" width="200" height="200" />
+//                 <img src="m2.png" width="200" height="200" />
+//                 <img src="m3.png" width="200" height="200" />
+//                 <img src="m4.png" width="200" height="200" />
+//             </marquee>
+//         </div>
+//     );
+// }
+
+// Key events
 export function EventDemo(){
-    function handleMouseOver(e) {
-        e.target.stop();
-    }
-    function handleMouseOut(e) {
-        e.target.start();
+    const [userId,setUserId] = useState('');
+    const [users,setUsers] = useState([]);
+    const [userError,setUserError] = useState('');
+    const [valClass, setValClass] = useState('');
+    useEffect(()=>{
+       axios.get("users.json").then((response)=>{
+            setUsers(response.data);
+       }) 
+    },[])
+    function VerifyUserId(e) {
+        for (var user of users) {
+            if (user.UserId === e.target.value) {
+                setUserError("User Id taken, Please try another.");
+                setValClass('text-danger');
+                break;
+            } else {
+                setUserError("User Id Available");
+                setValClass('text-success');
+            }
+        }
     }
     return (
-        <div className="container-fluid m-2 p-4">
-            <marquee scrollamount="20" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} style={{padding:'100px'}} >
-                <img src="m1.png" width="200" height="200" />
-                <img src="m2.png" width="200" height="200" />
-                <img src="m3.png" width="200" height="200" />
-                <img src="m4.png" width="200" height="200" />
-            </marquee>
+        <div className="container-fluid m-2" >
+            <h2>Register User</h2>
+            <dl>
+                <dt>User Id</dt>
+                <dd><input type="text" onKeyUp={VerifyUserId} /></dd>
+                <dd className={valClass}>{userError}</dd>
+
+            </dl>
         </div>
-    );
+     );
 }
