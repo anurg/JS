519f25c9d3dca556d1040aeeafa1256f74ce6d1f
anurg <anurg@yahoo.com>
2024-02-16 12:57:47 +0530

    Clock Demo using SetInterval.

--- react-projects/shopping-react/src/components/timer-demo/time-demo.jsx
+++ react-projects/shopping-react/src/components/timer-demo/time-demo.jsx
@@ -1,32 +1,52 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
+
+// export function TimerDemo() {
+//     const [vol,setVol] = useState(0);
+//     let vol2ref = useRef(null);
+//     function V1(){
+//         setVol(10);
+//     }
+//     function V2(){
+//         setVol(20);
+//     }
+//     function V3(){
+//         setVol(30);
+//     }
+//     function handleVolumeUp() {
+//        setTimeout(V1,5000);
+
+//       vol2ref.current = setTimeout(V2,7000);
+//        setTimeout(V3,9000);
+//     }
+//     function handleCancel() {
+//         clearTimeout(vol2ref.current);
+//     }
+//     return (
+//         <div className="container">
+//             <h2>Timer Demo</h2>
+//             <button className="bi bi-volume-up btn btn-primary me-3" onClick={handleVolumeUp}> + </button>
+//             <button className="bi bi-volume-down btn btn-primary" onClick={handleCancel}> Cancel 20% </button>
+//             <p>Vol increased by {vol} </p>
+//         </div>
+//     );
+// }
+
+//  Set Interval Demo
 
 export function TimerDemo() {
-    const [vol,setVol] = useState(0);
-    let vol2ref = useRef(null);
-    function V1(){
-        setVol(10);
-    }
-    function V2(){
-        setVol(20);
-    }
-    function V3(){
-        setVol(30);
-    }
-    function handleVolumeUp() {
-       setTimeout(V1,5000);
+    const [time,setTime] = useState();
 
-      vol2ref.current = setTimeout(V2,7000);
-       setTimeout(V3,9000);
+    function Clock() {
+        var now = new Date();
+
+        setTime(now.toLocaleTimeString())
     }
-    function handleCancel() {
-        clearTimeout(vol2ref.current);
-    }
+    useEffect(()=>{
+        setInterval(Clock,1000);
+    },[]); 
     return (
         <div className="container">
-            <h2>Timer Demo</h2>
-            <button className="bi bi-volume-up btn btn-primary me-3" onClick={handleVolumeUp}> + </button>
-            <button className="bi bi-volume-down btn btn-primary" onClick={handleCancel}> Cancel 20% </button>
-            <p>Vol increased by {vol} </p>
+            <p className="mt-4 text-center">{time}</p>
         </div>
     );
 }
