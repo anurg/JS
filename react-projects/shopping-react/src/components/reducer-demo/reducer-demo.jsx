4b9d7afeefd921cd60ea0ccdefa53c7428a421d8
anurg <anurg@yahoo.com>
2024-02-26 11:52:33 +0530

    formatting notes.

---
+++ react-projects/shopping-react/src/components/reducer-demo/reducer-demo.jsx
@@ -0,0 +1,11 @@
+
+export function ReducerDemo() {
+    return (
+        <div className="container-fluid">
+            <h3>Shopping</h3>
+            <button className="btn btn-primary me-4 bi bi-cart4"></button>
+            <button className="btn btn-danger bi bi-trash-fill"></button>
+        </div>
+    );
+
+}
--- react-projects/shopping-react/src/index.js
+++ react-projects/shopping-react/src/index.js
@@ -20,11 +20,12 @@ import { FormDemo } from "./components/form-demo/form-demo";
 import { HookForm } from "./components/hook-form/hook-form";
 import { CustomDemo } from "./components/custom-demo/custom-demo";
 import { ContextDemo } from "./components/context-demo/context-demo";
+import { ReducerDemo } from "./components/reducer-demo/reducer-demo";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <ContextDemo />
+    <ReducerDemo />
   </React.StrictMode>
 );
 
